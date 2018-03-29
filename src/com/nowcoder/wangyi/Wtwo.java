7cf48ad39d0a527678a59169c402fe2bc40bbd9f
chenrui <chenrui0227@gmail.com>
2018-03-29 21:17:37 +0800

    2018.3.29 21.17

---
+++ src/com/nowcoder/wangyi/Wtwo.java
@@ -0,0 +1,26 @@
+package com.nowcoder.wangyi;
+
+import java.util.Scanner;
+
+/**
+ * Created by ChenRui on 2018/3/27
+ */
+public class Wtwo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int count = 0;
+        for (int i = k; i <= n; i++) {
+            for (int j = k; j <= n; j++) {
+                if (i <= n && j <= n && i % j >= k) {
+                    ++count;
+                    System.out.println(i + " "+j);
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
